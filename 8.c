eabcd00201da94c321a99e76900ed8e860b16ebb
bibhash1996 <bibhash1996@gmail.com>
2017-01-13 21:14:51 +0530

    Perspective viewing

---
+++ 8.c
@@ -0,0 +1,96 @@
+GLfloat vertices8[][3]={{-1.0,-1.0,-1.0},{1.0,-1.0,-1.0},
+          {1.0,1.0,-1.0},{-1.0,1.0,-1.0},{-1.0,-1.0,1.0},
+           {1.0,-1.0,1.0},{1.0,1.0,1.0},{-1.0,1.0,1.0}};
+GLfloat colors8[][3]={{0.0,0.0,0.0},{1.0,0.0,0.0},
+              {1.0,1.0,0.0},{0.0,1.0,0.0},{0.0,0.0,1.0},
+              {1.0,0.0,1.0},{1.0,1.0,1.0},{0.0,1.0,1.0}};
+
+void polygon8(int a, int b, int c, int d)
+{
+	glBegin(GL_POLYGON);
+		glColor3fv(colors8[a]);
+		glVertex3fv(vertices8[a]);
+		glColor3fv(colors8[b]);
+		glVertex3fv(vertices8[b]);
+		glColor3fv(colors8[c]);
+		glVertex3fv(vertices8[c]);
+		glColor3fv(colors8[d]);
+		glVertex3fv(vertices8[d]);
+	glEnd();
+}
+
+void colorcube8()
+{
+	polygon8(0,3,2,1);
+	polygon8(2,3,7,6);
+	polygon8(0,4,7,3);
+	polygon8(1,2,6,5);
+	polygon8(4,5,6,7);
+	polygon8(0,1,5,4);
+}
+
+static GLfloat theta8[]={0.0,0.0,0.0};
+
+static GLdouble viewer[]={0.0,0.0,5.0};
+
+void disp8(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	gluLookAt(viewer[0], viewer[1],viewer[2],0.0,0.0,0.0,0.0,1.0,0.0);
+	glRotatef(theta8[0],1.0,0.0,0.0);
+	glRotatef(theta8[1],0.0,1.0,0.0);
+	glRotatef(theta8[2],0.0,0.0,1.0);
+	colorcube8();
+	glFlush();
+	glutSwapBuffers();
+}
+
+void mouse8(int btn, int state, int x, int y)
+{
+	if(btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN) axis=0;
+	if(btn==GLUT_MIDDLE_BUTTON&&state==GLUT_DOWN) axis=1;
+	if(btn==GLUT_RIGHT_BUTTON&&state==GLUT_DOWN) axis=2;
+	theta8[axis]+=2.0;
+	if(theta8[axis]>360.0) theta8[axis]-=360.0;
+	glutPostRedisplay();
+}
+
+void keys8(unsigned char key, int x, int y)
+{
+	if(key == 'x') viewer[0]-=1.0;
+	if(key == 'X') viewer[0]+=1.0;
+	if(key == 'y') viewer[1]-=1.0;
+	if(key == 'Y') viewer[1]+=1.0;
+	if(key == 'z') viewer[2]-=1.0;
+	if(key == 'Z') viewer[2]+=1.0;
+	glutPostRedisplay();
+}
+
+void myReshape8(int w, int h)
+{
+	glViewport(0,0,w,h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if(w<=h)
+        glFrustum(-2.0,2.0,-2.0*(GLfloat)h/(GLfloat)w,
+                 2.0*(GLfloat)h/(GLfloat)w, 2.0,20.0);
+	else
+        glFrustum(-2.0,2.0,-2.0*(GLfloat)w/(GLfloat)h,
+2.0*(GLfloat)w/(GLfloat)h, 2.0,20.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+/*void prog8()
+{
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(420.0,100.0);
+	glutCreateWindow("Colorcube Viewer");
+	glutReshapeFunc(myReshape8);
+	glutDisplayFunc(disp8);
+	glutMouseFunc(mouse8);
+	glutKeyboardFunc(keys8);
+	glEnable(GL_DEPTH_TEST);
+	glutMainLoop();
+}*/
